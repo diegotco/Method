c5c3837107dbb224ddd95dcb58346f12441b4a14
diegotco <diegotco@yahoo.com>
2021-02-10 12:39:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) throws Exception {
+        
+        gato();
+    }
+    
+    public static void gato (){
+        
+       String numeroVeces = "x3";
+        System.out.println("Miau " + numeroVeces);
+    }
+}
